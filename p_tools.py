b278310940c41a2a31a5e216f1cad2f5e497e3f2
ellisK-L <ellis.keener.lacroix@gmail.com>
2026-05-05 23:27:27 -0300

    changed p_tools to use new dir

--- p_tools.py
+++ p_tools.py
@@ -4,4 +4,4 @@ from PReader.pdecoder import PDecoder
 if __name__ == '__main__':
     decoder = PDecoder()
     decoder.debug_level = 1
-    decoder.raw_to_pandas("data/dat_1336.p")
+    decoder.raw_to_pandas("examples/raw/dat_1336.p")
